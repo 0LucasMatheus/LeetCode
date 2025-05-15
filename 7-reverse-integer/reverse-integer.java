ba63b2c490810288c98835a6942a00e6ade1b95b
Lucas Matheus Ferreira da Silva <136836943+0LucasMatheus@users.noreply.github.com>
2025-05-15 14:59:50 -0300

    Time: 2 ms (11.90%) | Memory: 41.7 MB (6.16%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int reverse(int x) {
+        //verifica se é negativo
+        Boolean negativo = x<0;
+        //vira string
+        String S = Integer.toString(x);
+        if (negativo){
+            //tira o - se for negativo
+            S = S.substring(1);
+        }
+        //inverte
+        String Sinvertido = new StringBuilder(S).reverse().toString();
+        try {
+            //vira int de volta
+            int intinvertido = Integer.parseInt(Sinvertido);
+            //return com operador ternario
+            return negativo ? -intinvertido : intinvertido;
+        } catch (NumberFormatException e) {
+            //se o numero for maior q um int ele pega o erro e retorna 0
+            return 0;
+        }
+    }
+}
