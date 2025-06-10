18fb5d0cc59fc722d391a64d2f8c3c17284842a5
Lucas Matheus Ferreira da Silva <136836943+0LucasMatheus@users.noreply.github.com>
2025-06-10 14:23:36 -0300

    Time: 0 ms (100.00%) | Memory: 42 MB (32.50%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,8 +1,11 @@
 class Solution {
     public int lengthOfLastWord(String s) {
         int count = 0;
+        //remove os espaços das pontas se existirem
         s=s.trim();
+        //começa o indice pelo ultimo da string
         int i = s.length() -1;
+        //verifica se o caractere é um espaço e desce 1 no indice
         while(i>=0 && s.charAt(i)!=' '){
             count++;
             i--;
